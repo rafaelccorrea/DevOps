9442980758a8095c26408e5cfdfacfd83cec18f0
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2022-10-20 23:00:04 -0300

    Add swarm

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]],["vagrant-winnfsd",["= 1.4.0"]]],"checksum":"fa61014a3060fbb8457eb3c4d838f27b3bd62abac1ceac3c8d0945fbc30c0ad7","vagrant_version":"2.3.0"}
